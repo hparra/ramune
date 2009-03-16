077ec25a1f8addbd99c5c0b26069947d91453b44
Hector G. Parra <hgparra+github@gmail.com>
2009-03-16 10:24:47 -0700

    Added VISCACommands.js and ramune.js functions to represent WENDI access.

--- bin-debug/javascripts/ramune.js
+++ bin-debug/javascripts/ramune.js
@@ -2,3 +2,23 @@ function trace(msg) {
 	//if (frescolita.use_trace)
 		$.jGrowl(msg);
 }
+
+
+function moveCamUp() {
+	trace("moveCamUp");
+}
+
+
+function moveCamDown() {
+	trace("moveCamDown");
+}
+
+
+function moveCamLeft() {
+	trace("moveCamLeft");
+}
+
+
+function moveCamRight() {
+	trace("moveCamRight");
+}
