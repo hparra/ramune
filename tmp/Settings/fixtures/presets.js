79b8f810e08b56c2ff8017f17c7e2dc1e88ed24a
Hector G. Parra <hparra@calit2.uci.edu>
2009-04-20 17:59:19 -0700

    In middle of code audit for Ramune Settings.

---
+++ tmp/Settings/fixtures/presets.js
@@ -0,0 +1,51 @@
+var ramuneSettings = {
+	// Presets
+	"presets_select" : "default",
+	
+	// Camera
+	"camera_select" : "default",
+	"resolution" : "640,480",
+	"quality-slide" : 50,
+	"framerate-slide" : 30,
+	
+	"keyframe" : 30,
+	"bandwidth" : 0,
+	
+	// Mic
+	"mic_select" : "default",
+	"encode-quality-slide" : 10,
+	"echo_suppression" : "true",
+	"gain-slide" : 50,
+	
+	"rate_select" : "22kHz",
+	"codec_select" : "NellyMosser",
+	"FPP" : 20,
+	"silence_timeout" : 0,
+	"silence_level" : 0
+};
+
+var testObject = {
+	// Presets
+	"presets_select" : "default",
+	
+	// Camera
+	"camera_select" : "default",
+	"resolution" : "640,480",
+	"quality-slide" : 50,
+	"framerate-slide" : 30,
+	
+	"keyframe" : 30,
+	"bandwidth" : 0,
+	
+	// Mic
+	"mic_select" : "default",
+	"encode-quality-slide" : 10,
+	"echo_suppression" : "true",
+	"gain-slide" : 50,
+	
+	"rate_select" : "22kHz",
+	"codec_select" : "NellyMosser",
+	"FPP" : 20,
+	"silence_timeout" : 0,
+	"silence_level" : 0
+};
