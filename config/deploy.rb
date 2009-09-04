1261d24828e358fa33ea307460fa180ac01c3666
Hector G. Parra <hgparra+github@gmail.com>
2009-09-03 18:37:57 -0700

    More flexibilty in cap config.

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,14 +2,20 @@ default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
 set :application, "ramune"
-set :domain, "marco.calit2.uci.edu"
+
+set(:domain) do
+  host = Capistrano::CLI.ui.ask "host: "
+  host += ".calit2.uci.edu"
+end
 
 # this does NOT deploy via git
 set :scm, :none
-set :repository, 'bin-release/'
+set :repository, 'release/'
 set :deploy_via, :copy
 
-set :user, "marco"
+set(:user) do
+  Capistrano::CLI.ui.ask "login: "
+end
 set :use_sudo, false
 set :deploy_to, "/var/www/#{application}"
 
